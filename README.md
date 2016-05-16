96c3c00ffab103908dc6cfbbd64b56247eaea2a9
Jose Gordillo <josegordillo@Joses-MacBook-Pro.local>
2016-05-15 21:14:51 -0700

    added initial files for personal webiste using nodejs

---
+++ README.md
@@ -0,0 +1,5 @@
+Personal website
+
+Author: Jose Javier Gordillo
+
+Created from scratch using jQuery and Bootstrap 
---
+++ public/scripts/contentHandler.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	var option, 
+		url = window.location.href,
+		$mainContainer = $('#centered-content'); 
+
+	option = url.match(/option=(.*)/);
+
+	if (option === null) {
+		showDiv("skills", $mainContainer);
+	} else {
+		showDiv(option[1], $mainContainer);
+	}
+});
+
+function showDiv(option, container) {
+	container.find('#' + option + "-content").show();
+}
+
