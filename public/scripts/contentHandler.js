c11562bac53d0f6e6a9cd0729744f64cddaf2c70
Jose Gordillo <gordillo@uvic.ca>
2016-05-23 16:07:33 -0700

    added image list for projects.

--- public/scripts/contentHandler.js
+++ public/scripts/contentHandler.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	var option, 
+		project,
 		url = window.location.href,
 		$mainContainer = $('#centered-content'); 
 
@@ -8,12 +9,27 @@ $(document).ready(function() {
 
 	if (option === null) {
 		showDiv("main-page", $mainContainer);
-	} else {
-		showDiv(option[1], $mainContainer);
+	} 
+	else {
+		if(option[1].indexOf('projects') > -1){
+			project = option[1].split("-");
+			showDiv(project[0], $mainContainer);
+			if(project.length > 1){
+				
+				scrollToAnchor(project[1]+"-project");
+			}
+		} else {
+			showDiv(option[1], $mainContainer);
+		}
 	}
 });
 
 function showDiv(option, container) {
 	container.find('#' + option + "-content").show();
+}
+
+function scrollToAnchor(anchor_id){
+    var tag = $("#"+anchor_id);
+    $('html,body').animate({scrollTop: tag.offset().top},'slow');
 }
 
